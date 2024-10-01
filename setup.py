1e2da281b1f0bc97cbbf7aad7cd3bac32a6cbb97
Jonty Marshall <32949295+jontymarshall@users.noreply.github.com>
2024-10-01 14:29:01 +0800

    Fixed typos, updated setup

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup,find_packages
 
 setup(name='pysed',
-	version='1.0',
+	version='0.0',
 	description='SED fitting or stellar photometry',
-	author="Zachary Draper",
-	author_email='zackdraper@gmail.com',
-	url='',
+	author='Jonathan P. Marshall',
+	author_email='jmarshall@asiaa.sinica.edu.tw',
+	original_author='Zachary Draper',
+	url='https://github.com/jontymarshall/pysed/',
 	packages=find_packages(),
     	classifiers=[
 		'Intended Audience :: Science/Research',
@@ -16,7 +17,7 @@ setup(name='pysed',
 		'Programming Language :: Python :: 2.7',
 		'Programming Language :: Python :: 3.5',
 		],
-	install_requires=['numpy', 'scipy', 'astropy', 'astroquery', 'matplotlib']
+	install_requires=['numpy', 'scipy', 'astropy', 'astroquery', 'matplotlib', 'extinction']
 )
 
 
